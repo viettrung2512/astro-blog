b71a645350a79f6b7615ab61f1d7e68374745b97
NguyenVietTrung <117735066+viettrung2512@users.noreply.github.com>
2025-06-03 15:26:05 +0700

    add netlify, unistall vercal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel()
 });
